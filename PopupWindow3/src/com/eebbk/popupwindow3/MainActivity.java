cd8429759e45980aa79f260a3b1ca5278aafd8d5
iPlanGroup <iPlanGroup@126.com>
2013-08-24 21:45:25 +0800

    change test, third time
    33333333333333333333333333333

--- PopupWindow3/src/com/eebbk/popupwindow3/MainActivity.java
+++ PopupWindow3/src/com/eebbk/popupwindow3/MainActivity.java
@@ -56,6 +56,8 @@ public class MainActivity extends Activity implements OnClickListener
 				childItemList.add(childMap);
 			}
 			childList.add(childItemList);
+
+			System.out.println("test again third time");
 		}
 		
 //		adapter = new ExListAdapter(this, groupList, childList);
